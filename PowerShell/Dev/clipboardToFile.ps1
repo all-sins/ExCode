8b7161e2264353481d34f1bb10e0df66e63a61b4
all-sins <arturs.alsins@tutanota.com>
2022-07-06 14:04:52 +0300

    clipboardToFile and psCliInvokeFileFromRepo as courtesy of early convo with Eddy.

---
+++ PowerShell/Dev/clipboardToFile.ps1
@@ -0,0 +1,1 @@
+New-Item -Type File psCliInvokeFileFromRepo.ps1; Get-Clipboard | Set-Content -Path .\psCliInvokeFileFromRepo.ps1;
---
+++ PowerShell/Prod/psCliInvokeFileFromRepo.ps1
@@ -0,0 +1,1 @@
+cd $env:TEMP; mkdir ExCodeTMP; cd ExCodeTMP; git clone "https://github.com/all-sins/ExCode.git" .; .\PowerShell\Dev\publicipmon.ps1; cd ..; Remove-Item -Force -Recurse .\ExCodeTMP"
