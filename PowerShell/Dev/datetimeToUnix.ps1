082e8593a3c360557404ce2f858a9965763a48c7
unknown <tsukiakari@tutanota.com>
2023-12-13 18:03:49 +0200

    Q4 and datetimeToUnix added.

---
+++ PowerShell/Dev/datetimeToUnix.ps1
@@ -0,0 +1,17 @@
+﻿function datetimeToUnix {
+  param([DateTime] $dt)
+  $universal = $dt.ToUniversalTime()
+  $daylightsavings = $universal.AddHours(2)
+  $subtractEpoch = $daylightsavings.Subtract((Get-Date "1970-01-01"))
+  return $subtractEpoch.TotalSeconds
+}
+
+
+$tmp = New-Object DateTime
+$tmp = $tmp.AddYears(2022) # start
+Write-Host "Start: $(datetimeToUnix -dt $tmp)"
+
+$tmp = $tmp.AddHours(23)
+$tmp = $tmp.AddMinutes(59)
+$tmp = $tmp.AddSeconds(59)
+Write-Host "End: $(datetimeToUnix -dt $tmp)"
---
+++ PowerShell/Dev/gravityTeamQ4.ps1
@@ -0,0 +1,117 @@
+﻿###############################
+# Consider rewriting in Java. #
+###############################
+$max = [Math]::Pow(10,12) 
+$maxPerCore = $max / 8 # 125000000
+$threads = 8
+$analyticsThrottle = 5
+Write-Host $maxPerCore
+# Why are you not getting populated with data?
+$sums = @([System.Int64]::new()) * 8
+
+# Create a mutex for synchronization
+$mutex = [System.Threading.Mutex]::new()
+
+# Function to increment an element in the array in a synchronized manner
+function AddValueToIndex {
+    param([System.Int64]$value, [int]$index)
+    $mutex.WaitOne() | Out-Null
+    $sums[$index] = $sums[$index] + $value
+    $debugInfo = "Thread-$index Updated sums[$index]: $($sums[$index])"
+    $mutex.ReleaseMutex()
+    Out-File -FilePath C:\Users\Tsu\add-value.log -Append -InputObject $debugInfo
+}
+
+# Function to increment an element in the array in a synchronized manner
+function GetValueAtIndex {
+    param([int]$index)
+    $mutex.WaitOne() | Out-Null
+    $returnTmp = $sums[$index]
+    $debugInfo = "Thread-$index Returning sums[$index]: $returnTmp"
+    $mutex.ReleaseMutex()
+    Out-File -FilePath C:\Users\Tsu\get-value.log -Append -InputObject $debugInfo
+    return $returnTmp
+}
+
+# Create and or clear log files.
+if (Test-Path -Path C:\Users\Tsu\add-value.log) {
+    Clear-Content -Path C:\Users\Tsu\add-value.log
+} else {
+    New-Item -ItemType File -Name add-value.log -Path C:\Users\Tsu\
+}
+
+if (Test-Path -Path C:\Users\Tsu\get-value.log) {
+    Clear-Content -Path C:\Users\Tsu\get-value.log
+} else {
+    New-Item -ItemType File -Name get-value.log -Path C:\Users\Tsu\
+}
+
+if (Test-Path -Path C:\Users\Tsu\compute.log) {
+    Clear-Content -Path C:\Users\Tsu\compute.log
+} else {
+    New-Item -ItemType File -Name compute.log -Path C:\Users\Tsu\
+}
+
+# Define compute thread.
+$computeJob = {
+    param($start, $end, $index)
+    Out-File -FilePath C:\Users\Tsu\tmp.file -Append -InputObject "$start $end $index"
+    for ($i = $start; $i -lt $end; $i++) {
+        Out-File -FilePath C:\Users\Tsu\tmp.file -Append -InputObject "Computing $i"
+        $asStr = [String]$i
+        [System.Int64]$tmp = 0
+        foreach ($char in $asStr.ToCharArray()) {
+            $tmp = $tmp + [int]::Parse($char)
+        }
+        Out-File -FilePath C:\Users\Tsu\tmp.file -Append -InputObject "Storing $i => $tmp"
+        AddValueToIndex -value $tmp -index $index
+    }
+}
+
+# Start jobs in parallel
+$msg = ""
+for ($j = 0; $j -lt $threads; $j++) {
+    $start = $j * $maxPerCore
+    $end = (($j + 1) * $maxPerCore) - 1
+    if (($end -gt $max) -or ($j -eq $threads - 1)) {
+      $end = $max
+    }
+    Start-Job -ScriptBlock $computeJob -ArgumentList $start, $end, $j
+    $msg += "[Thread-$j] $start $end`n"
+}
+Write-Host -ForegroundColor Cyan $msg
+
+# Wait for all jobs to finish and print analytic data
+Write-Host -ForegroundColor Magenta "[ANALYTICS] Starting..."
+$analyticsItteration = 0
+while (Get-Job -State "Running") {
+    $currentSUM = 0
+    $tmp = @(0) * 8
+    for ($i = 0; $i -lt 8; $i++) {
+        $tmp[$i] = GetValueAtIndex -index $i
+        $currentSUM += $tmp[$i]
+        Write-Host -ForegroundColor Cyan -NoNewline "[Thread-$i] "
+        Write-Host -ForegroundColor Blue $tmp[$i]
+    }
+    Write-Host "CurrentSUM: " $currentSUM
+    Write-Host
+    Start-Sleep -Seconds $analyticsThrottle
+    $analyticsItteration++
+}
+
+Write-Host -ForegroundColor Green "COMPUTE-DONE!"
+Write-Host -ForegroundColor "Writing results to file..."
+if (-not (Test-Path -Path ~\gravityTeamQ4.result)) {
+  New-Item -ItemType File -Path ~\ -Name gravityTeamQ4.result
+}
+Clear-Content -Path ~\gravityTeamQ4.result
+# Retrieve results and save them.
+Set-Content -Path ~\gravityTeamQ4.result -Value $(Receive-Job -Keep)
+Write-Host -ForegroundColor Green "WRITE-DONE!"
+
+# Clean-up threads.
+Write-Host -ForegroundColor "Removing Jobs..."
+foreach ($job in Get-Job) {
+  Stop-Job $job
+  Remove-Job $job
+}
