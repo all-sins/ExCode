6f033c6570dc45d4dfa62ca47a376968a15c53a2
Tsu <tsukiakari@tutanota.com>
2024-06-09 15:50:03 +0300

    Added some QOL Ubuntu-based install scripts for common software.

---
+++ Bash/LinuxScripts/install-spotify.sh
@@ -0,0 +1,3 @@
+curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
