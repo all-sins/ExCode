3966a4b814a191d27230a02773ec51348c79d5eb
all-sins <tsukiakari@tutanota.com>
2024-07-12 12:14:58 +0300

    Renamed old and repurposed copy into MC color code generator for MOTDs.

---
+++ PowerShell/Dev/minecraftRGBText-EscapeCodes.ps1
@@ -0,0 +1,18 @@
+﻿# Define command-line arguments.
+param (
+    # Text to add RGB to.
+    [string]$inputString = "Lorem ipsum!"
+)
+
+# Gold, Blue, Green, Aqua, Red, Light Purple, White respectively.
+$pchar = @("\u00A76","\u00A79","\u00A7a","\u00A7b","\u00A7c","\u00A7d","\u00A7f")
+
+$rgbAppliedString = ""
+$lastRand = ""
+for ($i = 0; $i -lt $inputString.Length; $i++) {
+    do {$rand = Get-Random -InputObject $pchar} while ($rand -eq $lastRand)
+    $rgbAppliedString = $rgbAppliedString+$schar+$rand+$inputString.Substring($i, 1)
+    $lastRand = $rand
+}
+Write-Host $rgbAppliedString
+$rgbAppliedString | Set-Clipboard
